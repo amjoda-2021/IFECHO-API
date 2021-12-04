9dd2d70ccf576481800b06645ae64fbf0c0e12aa
AmelieLoulergue <loulergueamelie@yahoo.fr>
2021-12-04 20:44:55 +0100

    seed

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,27 +1,34 @@
 # frozen_string_literal: true
 class SitesController < ApplicationController
+  # before_action :authenticate_user!
   before_action :set_site, only: %i[show update destroy]
-  before_action :authenticate_user!
 
   # GET /sites
   def index
-    # @sites = Site.all
-    @sites = current_user.sites
+    @sites = Site.all
+    # @sites = current_user.sites
 
     render json: @sites
   end
 
   # GET /sites/1
   def show
-    reference_date = '2018-06-20'.to_date
+    reference_date = '2019-06-26'.to_date
     number_days_back = 5
     number_days_forward = 5
+   
     past_thi_array = HistoricalThi.new(@site, number_days_back, reference_date).perform
     past_ct_array = HistoricalCt.new(@site, number_days_back, reference_date).perform
     future_thi_array = PredictedThi.new(@site, number_days_forward, reference_date).perform
     future_ct_array = PredictedCt.new(@site, number_days_forward, reference_date).perform
+    puts "*"* 100
+    puts @site
+    puts "*"* 100
+    puts past_thi_array
+    puts "*"* 100
     render json: { site: @site, historical_thi: past_thi_array, historical_ct: past_ct_array,
                    future_thi: future_thi_array, future_ct: future_ct_array }
+                  
   end
 
   # POST /sites
@@ -53,13 +60,13 @@ class SitesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_site
-    # @site = Site.find(params[:id])
-    @site = current_user.sites.find(params[:id])
+    @site = Site.find(params[:id])
+    # @site = current_user.sites.find(params[:id])
 
   end
 
   # Only allow a list of trusted parameters through.
   def site_params
-    params.fetch(:site, {})
+    params.require(:user).permit(:name, :site_type, :breeder_id, :location, :city, :post_code, :longitude, :latitude)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,25 +10,25 @@
 
 require 'csv'
 
-# [CtDatum, SiteAdvisor, Site, ThiDatum, User].map(&:destroy_all)
+[CtDatum, SiteAdvisor, Site, ThiDatum, User].map(&:destroy_all)
 [ThiDatum].map(&:destroy_all)
 
-# %w[ct_data site_advisors sites thi_data users].map do |tab|
-#   ActiveRecord::Base.connection.reset_pk_sequence!(tab)
-# end
+%w[ct_data site_advisors sites thi_data users].map do |tab|
+  ActiveRecord::Base.connection.reset_pk_sequence!(tab)
+end
 
 %w[thi_data].map do |tab|
   ActiveRecord::Base.connection.reset_pk_sequence!(tab)
 end
 
-# User.create(first_name: 'jo', last_name: 'justman',
-#             email: 'jojo.justman@gmail.com', password: '123456', is_advisor?: true)
+User.create(first_name: 'Arti', last_name: 'Chaud',
+            email: 'artichaud@gmail.com', password: '123456')
 
 # bdd_meteo = CSV.read('app/assets/mailleLONLATALT.csv',
 #                      headers: true, liberal_parsing: true)
 
-# bdd_farms = CSV.read('app/assets/fermes_exp.csv',
-#                      headers: true, liberal_parsing: true)
+bdd_farms = CSV.read('app/assets/fermes_exp.csv',
+                     headers: true, liberal_parsing: true)
 
 bdd_thi = CSV.read('app/assets/thi.csv',
                    headers: true, liberal_parsing: true)
@@ -42,11 +42,17 @@ bdd_ct = CSV.read('app/assets/ct.csv',
 #               longitude: (row2[2].to_f * 10_000).to_i, site_type: 'meteo')
 # end
 
-# bdd_farms.each do |row|
-#   row2 = row[0].split(';')
-#   Site.create(name: row2[0], longitude: (row2[1].to_f * 10_000).to_i,
-#               latitude: (row2[2].to_f * 10_000).to_i, site_type: 'production')
-# end
+bdd_farms.each do |row|
+  row2 = row[0].split(';')
+  Site.create(name: row2[0], longitude: (row2[1].to_f * 10_000).to_i,
+              latitude: (row2[2].to_f * 10_000).to_i, site_type: 'production')
+end
+
+Site.first.update(breeder:User.first)
+Site.find(2).update(breeder:User.first)
+SiteAdvisor.create(advised_site: Site.find(3), advisor: User.first)
+SiteAdvisor.create(advised_site: Site.find(4), advisor: User.first)
+
 
 bdd_thi.each do |row|
   row2 = row[0].split(';')
