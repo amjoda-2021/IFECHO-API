98a87c0c97581eee8469205ba8c1f5e1cd42a884
David Guetta <davguetta@gmail.com>
2021-12-04 23:20:40 +0100

    refacto: adding ReturnThermalData class to manage complete data set up process

---
+++ app/services/return_thermal_data.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+class ReturnThermalData
+  def initialize(site, number_days_back, number_days_forward, reference_date)
+    @site = site
+    @number_days_back = number_days_back
+    @number_days_forward = number_days_forward
+    @reference_date = reference_date.to_date
+    @final_data_set = { historical_thi: [], predicted_thi: [], historical_ct: [], predicted_ct: [] }
+  end
+
+  def perform
+    @final_data_set[:historical_thi] = HistoricalThi
+                                       .new(@site, @number_days_back, @reference_date).perform
+    @final_data_set[:predicted_thi] = PredictedThi
+                                      .new(@site, @number_days_forward, @reference_date).perform
+    @final_data_set[:historical_ct] = HistoricalCt
+                                      .new(@site, @number_days_back, @reference_date).perform
+    @final_data_set[:predicted_ct] = PredictedCt
+                                     .new(@site, @number_days_forward, @reference_date).perform
+    complete_ct_dataset
+  end
+
+  private
+
+  def complete_ct_dataset
+    ct_past_dataset_length = @final_data_set[:historical_ct].count
+    ct_future_dataset_length = @final_data_set[:predicted_ct].count
+    initial_not_a_number_array = Array.new(ct_past_dataset_length)
+    final_not_a_number_array = Array.new(ct_future_dataset_length)
+    @final_data_set[:historical_ct] = @final_data_set[:historical_ct] + final_not_a_number_array
+    @final_data_set[:predicted_ct] = initial_not_a_number_array + @final_data_set[:predicted_ct]
+    @final_data_set
+  end
+end
